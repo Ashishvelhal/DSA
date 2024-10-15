749ff42440eb98d79678d0d7b2bb7c29e810ca50
ashish_velhal <ashishvelhal147@gmail.com>
2024-10-15 20:11:11 +0530

    Logical Operaters changes

--- logical.cpp
+++ logical.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main(){
     cout << ( (3 < 2) || (3 < 5) ) << endl;
+    cout << ( (3 < 2) && (3 < 5) ) << endl;
+    cout << ( (3 < 2) != (3 < 5) ) << endl;
     return 0;
 }
