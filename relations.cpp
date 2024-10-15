99c043a03f0f471fcd5b55ffc7ee949ae178ebc4
ashish_velhal <ashishvelhal147@gmail.com>
2024-10-15 20:04:25 +0530

    Relational Operaters

---
+++ relations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << ( 2 < 5 ) <<endl;
+    cout << ( 2 > 5 ) <<endl;
+    cout << ( 2 <= 5 ) <<endl;
+    cout << ( 2 >= 5 ) <<endl;
+    cout << ( 2 == 5 ) <<endl;
+    cout << ( 2 != 5 ) <<endl;
+
+    return 0;
+}
