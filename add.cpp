92759e0fde51f00cf322bde2702346ebaf02ae86
ashish_velhal <ashishvelhal147@gmail.com>
2024-10-15 19:55:51 +0530

    add,multiplication,div,sub

---
+++ add.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a = 10 , b = 10;
+    cout << "sum = " << (a+b) << endl;
+    return 0;
+}
---
+++ multi.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a = 10 , b = 10;
+    cout << "multiplication = " << (a*b) << endl;
+    return 0;
+}
